c5ecfe3b550812f45bb40283e25ceec80216c2bc
kmsshashki <59206214+kmsshashki@users.noreply.github.com>
2024-11-01 18:16:05 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,6 @@
 immutable_var = 1, 'd', True, [0, 1]
-#immutable_var[1] = 2 выдаст ошибку
+#immutable_var[1] = 2
+#Выдаст ошибку
 #Кортеж неизменяемый объект, изменить можно только элементы списка внутри кортежа.
 immutable_var[3][0] = 3
 print('Immutable tuple:', immutable_var)
