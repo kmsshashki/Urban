aafaa14d3e45d64dcc01d51c2b6eec5a90a6db5f
kmsshashki <59206214+kmsshashki@users.noreply.github.com>
2024-11-21 18:57:07 +0300

    Update module_2_3.py
    Задача исправлена. Индексы изменены

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
 while i < len(my_list):
-    i += 1
-    if my_list[i-1] > 0:
-        print(my_list[i-1])
-    elif my_list[i-1] == 0:
-        continue
-    else:
+    if my_list[i] > 0:
+        print(my_list[i])
+    elif my_list[i] < 0:
         break
+    i += 1
