820a152af58b7c1abc2ea280dea92f5f50d3ddba
kmsshashki <59206214+kmsshashki@users.noreply.github.com>
2024-11-26 23:03:01 +0300

    Update module_3_5.py
    Исправлена ошибка умножения на ноль.

--- module_3_5.py
+++ module_3_5.py
@@ -1,8 +1,12 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number = str(number) if number % 10 !=0 else str(number // 10)
     first = int(str_number[0])
-    if len(str_number) == 1:
-        return first
-    return first * get_multiplied_digits(int(str_number[1:]))
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    return first
+
+
 result = get_multiplied_digits(40203)
 print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
